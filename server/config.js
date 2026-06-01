81669eaf7f7d3620ae63dca4b99a37e7e0583242
meiller <meiller@mi-01.oth-aw.de>
2026-06-01 13:08:46 +0200

    env

--- server/config.js
+++ server/config.js
@@ -1,4 +1,7 @@
+// const LOCAL_MONGODB_URI = 'mongodb://localhost:27017/medienausleihe';
+
 module.exports = {
-  MONGODB_URI: 'mongodb://localhost:27017/medienausleihe',
+  // Wenn hier nichts eingetragen ist, wird MONGODB_URI aus den Umgebungsvariablen verwendet.
+  MONGODB_URI: LOCAL_MONGODB_URI || process.env.MONGODB_URI,
   PORT: 3002
 };
